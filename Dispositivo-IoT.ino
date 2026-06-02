357f0892dd561c218f658008a701bbdfbdac19d3
KelsonZh0 <kel180498@gmail.com>
2026-06-02 14:12:10 -0300

    feat: adiciona leitura de luminosidade com LDR

--- Dispositivo-IoT.ino
+++ Dispositivo-IoT.ino
@@ -3,10 +3,15 @@
 
 const int LED_VERMELHO_PIN = 25;
 const int LED_VERDE_PIN = 26;
+
 const int PH_PIN = 34;
+const int LDR_PIN = 35;
 
 const float PH_MIN_IDEAL = 6.5;
 const float PH_MAX_IDEAL = 8.5;
+
+const int LUMINOSIDADE_MIN_IDEAL = 250;
+const int LUMINOSIDADE_MAX_IDEAL = 900;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
@@ -16,25 +21,50 @@ float mapFloat(float valor, float entradaMin, float entradaMax, float saidaMin, 
 
 float lerPH() {
   int leituraADC = analogRead(PH_PIN);
-
   float ph = mapFloat(leituraADC, 0, 4095, 0.0, 14.0);
-
   return ph;
 }
 
-void atualizarDisplay(float ph, bool phNormal) {
+int lerLuminosidade() {
+  int leituraADC = analogRead(LDR_PIN);
+
+  // Escala didática: 0 a 1000
+  int luminosidade = map(leituraADC, 0, 4095, 0, 1000);
+
+  luminosidade = constrain(luminosidade, 0, 1000);
+
+  return luminosidade;
+}
+
+void atualizarAtuadores(bool sistemaNormal) {
+  if (sistemaNormal) {
+    digitalWrite(LED_VERDE_PIN, HIGH);
+    digitalWrite(LED_VERMELHO_PIN, LOW);
+  } else {
+    digitalWrite(LED_VERDE_PIN, LOW);
+    digitalWrite(LED_VERMELHO_PIN, HIGH);
+  }
+}
+
+void atualizarDisplay(float ph, int luminosidade, bool phNormal, bool luzNormal) {
   lcd.clear();
 
   lcd.setCursor(0, 0);
-  lcd.print("pH: ");
-  lcd.print(ph, 2);
+  lcd.print("pH:");
+  lcd.print(ph, 1);
+  lcd.print(" L:");
+  lcd.print(luminosidade);
 
   lcd.setCursor(0, 1);
 
-  if (phNormal) {
-    lcd.print("OK 6.5 - 8.5");
-  } else {
+  if (phNormal && luzNormal) {
+    lcd.print("STATUS: NORMAL");
+  } else if (!phNormal && !luzNormal) {
+    lcd.print("ALERTA PH/LUZ");
+  } else if (!phNormal) {
     lcd.print("ALERTA PH");
+  } else {
+    lcd.print("ALERTA LUZ");
   }
 }
 
@@ -44,7 +74,9 @@ void setup() {
 
   pinMode(LED_VERMELHO_PIN, OUTPUT);
   pinMode(LED_VERDE_PIN, OUTPUT);
+
   pinMode(PH_PIN, INPUT);
+  pinMode(LDR_PIN, INPUT);
 
   digitalWrite(LED_VERMELHO_PIN, LOW);
   digitalWrite(LED_VERDE_PIN, LOW);
@@ -66,18 +98,15 @@ void setup() {
 
 void loop() {
   float ph = lerPH();
+  int luminosidade = lerLuminosidade();
 
   bool phNormal = ph >= PH_MIN_IDEAL && ph <= PH_MAX_IDEAL;
+  bool luzNormal = luminosidade >= LUMINOSIDADE_MIN_IDEAL && luminosidade <= LUMINOSIDADE_MAX_IDEAL;
 
-  if (phNormal) {
-    digitalWrite(LED_VERMELHO_PIN, LOW);
-    digitalWrite(LED_VERDE_PIN, HIGH);
-  } else {
-    digitalWrite(LED_VERMELHO_PIN, HIGH);
-    digitalWrite(LED_VERDE_PIN, LOW);
-  }
+  bool sistemaNormal = phNormal && luzNormal;
 
-  atualizarDisplay(ph, phNormal);
+  atualizarAtuadores(sistemaNormal);
+  atualizarDisplay(ph, luminosidade, phNormal, luzNormal);
 
   delay(1000);
 }
